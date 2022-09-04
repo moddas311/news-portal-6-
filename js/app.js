887f72fda26330b2b109ac27e002fe7153e5a309
moddas311 <moddasir311@gmail.com>
2022-09-04 11:31:08 +0600

    ar pari na vai allah jane ki hobe amar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const loadCategories = async() =>{
-    const url = 'https://openapi.programming-hero.com/api/news/categories';
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
     displayCategories(data.data.news_category);
@@ -7,18 +7,58 @@ const loadCategories = async() =>{
 const displayCategories = (categories) =>{
     const newsCategory = document.getElementById('category-items');
     categories.forEach((category) => {
-        const createList = document.createElement('div');
-        createList.innerHTML =`
-        <a onclick="loadNews()" href = '#'>${category.category_name}</a>
+        const newsDiv = document.createElement('div');
+        newsDiv.innerHTML =`
+        <a  onclick="loadNews()" href = '#'>${category.category_name}</a>
         `;
-        newsCategory.appendChild(createList);
+        newsCategory.appendChild(newsDiv);
     });
 }
 
 const loadNews = async() =>{
+    const url = (`https://openapi.programming-hero.com/api/news/category/01`)
     const res = await fetch(url);
     const data = await res.json();
-    
+    displayNews(data.data[0])
 } 
+
+const displayNews = (news) =>{
+    console.log(news);
+    const allNews = document.getElementById('all-news');
+    const allNewsDiv = document.createElement('div');
+    allNewsDiv.innerHTML =`
+    <div class="flex card lg:card-side bg-base-full shadow-xl gap-10">
+                <figure><img src="${news.image_url}" alt="Album"></figure>
+                <div class="card-body">
+                    <h2 class="card-title text-3xl gap-2">${news.title}</h2>
+                    <p>${news.details}</p>
+                    <!-- --- -->
+                    <div class="flex justify-around">
+                        <div class="flex gap-5 content-center">
+                            <div class="dropdown dropdown-end">
+                                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                                    <div class="w-10 rounded-full">
+                                        <img src="${news.author.img}"/>
+                                    </div>
+                                </label>
+                            </div>
+                            <div class="form-control">
+                                <h5 class="text-xl">Name: ${news.author.name}</h5>
+                                <p>${news.author.published_date}</p>
+                            </div>
+                        </div>
+                        <!-- ---  -->
+                        <div class="justify-center">
+                            <h5 class="2xl">${news.rating.badge}</h5>
+                            <p class="text-center">${news.rating.number}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    `;
+    allNews.appendChild(allNewsDiv)
+
+}
+loadNews()
 
 loadCategories();
